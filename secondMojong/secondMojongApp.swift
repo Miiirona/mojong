8977e190c4c44f8f2af4f957f42deea1a9186232
Miiiro <miiiro.na95@gmail.com>
2023-11-16 18:53:18 +0900

    Initial Commit

---
+++ secondMojong/secondMojongApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondMojongApp.swift
+//  secondMojong
+//
+//  Created by Hyemi on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct secondMojongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
